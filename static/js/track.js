7e86c6834e640b959d04da409cca5af98688d345
xelA-S <asmith6398@gmail.com>
2021-08-16 18:40:26 +0100

    added for loop to render links in the array onto page

--- static/js/track.js
+++ static/js/track.js
@@ -1,11 +1,15 @@
 const inputElement=document.getElementById("input");
 const saveElement=document.getElementById("save");
+const listElement=document.getElementById("list");
 let Leads=[]
 
 
 
-saveElement.addEventListener("click",function(){
-    Leads.push(inputElement.value)
-})
 
- 
+saveElement.addEventListener("click",function(){
+    Leads.push(inputElement.value);
+    for(let i=0; i < Leads.length; i++){
+        listElement.innerHTML+="<li>" + Leads[i] + "</li>";
+    }
+    
+})
