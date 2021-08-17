285f6777b0d307264380910acb0bd598e8a634d6
xelA-S <asmith6398@gmail.com>
2021-08-17 15:16:14 +0100

    added delete all button and changed some css

--- static/js/track.js
+++ static/js/track.js
@@ -1,17 +1,22 @@
+let Links=["test.com"]
 const inputElement=document.getElementById("input");
 const saveElement=document.getElementById("save");
 const listElement=document.getElementById("list");
-let Links=[]
-let linksFromLocalStorage=JSON.parse(localstorage.getItem("links"))
+const DeleteElement=document.getElementById("delete");
+let linksFromLocalStorage = JSON.parse( localStorage.getItem("links") )
 
 if(linksFromLocalStorage){
     Links=linksFromLocalStorage
     renderLinks()
 }
 
+
+
+console.log(linksFromLocalStorage)
+
 saveElement.addEventListener("click",function(){
     Links.push(inputElement.value);
-    localStorage.setItem("links",JSON.stringify(Links))
+    localStorage.setItem("links", JSON.stringify(Links))
     inputElement.value=""
     renderLinks()
 })
@@ -23,6 +28,13 @@ inputElement.addEventListener("keypress",function(e){
         inputElement.value=""
         renderLinks()
     }
+})
+
+DeleteElement.addEventListener("dblclick",function( ){
+    localStorage.clear()
+    Links=[]
+    renderLinks()
+
 })
 
 
